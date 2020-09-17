44136dbef30bfa417e13b5f41411e7aa2a1e2e54
xiaocg-pc <386353564@qq.com>
2020-09-17 17:38:15 +0800

    ddd

---
+++ analysis/src/test/java/com/security/manage/analysis/AnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.manage.analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ search/src/test/java/com/security/manage/search/SearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.manage.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
